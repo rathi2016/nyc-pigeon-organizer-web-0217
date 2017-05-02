75bde7b973b9c2df2ee77c85bd7c6830f554418d
Rathi <github email address>
2017-05-02 10:33:39 -0400

    learnt about new ||= operator"

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, h), pigeon_list|
+    h.each do |value, names|
+      names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][key] ||= []
+        pigeon_list[name][key] << value.to_s
+      end
+    end
+  end
 end
